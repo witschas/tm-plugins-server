5025daa6fa8a4b2f65f7e5799435a0b3eb30ed07
Rico Witschas <rico@99coaches.com>
2025-12-21 18:58:45 +0100

    refactor: use process.cwd() for export paths for powerpointGenerator and wordGenerator

--- src/routes/wordGenerator/wordGeneratorRouter.ts
+++ src/routes/wordGenerator/wordGeneratorRouter.ts
@@ -45,7 +45,7 @@ wordGeneratorRegistry.registerPath({
 });
 
 // Create folder to contains generated files
-const exportsDir = path.join(__dirname, '../../..', 'word-exports');
+const exportsDir = path.join(process.cwd(), 'word-exports');
 // Ensure the exports directory exists
 if (!fs.existsSync(exportsDir)) {
   fs.mkdirSync(exportsDir, { recursive: true });
